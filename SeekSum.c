0c91fcfdfd6f18309a639f94883d1d7b661903b6
Firkraag <cntqrxj@gmail.com>
2016-02-04 15:32:34 +0800

    modify SeekSum.c

--- SeekSum.c
+++ SeekSum.c
@@ -3,18 +3,6 @@
 //determines whether or not there exist two elements in S whose sum is
 //exactly x. The set S must be already sorted
 
-//BinSearch(A, first, end, x)
-//	while length >= 0
-//		inter = (first + end) / 2
-//		if x == A[inter]
-//			return inter + 1
-//		else if x < A[inter]
-//			end = inter - 1
-//		else
-//			first = inter + 1
-//		length = end - first
-//	return -1
-	
 int BinSearch(int A[], int first, int end, int x) {
 	int length = end - first;
 	int inter;
@@ -45,8 +33,11 @@ void SeekSum(int A[], int first, int end, int x) {
 			 
 
 int main() {
-	int A[10] = {10,2,3,4,15,6,70,7,8,8};
+	int A[10] = {2,3,4,6, 7, 8, 8, 10, 15 , 70};
 	
 	SeekSum(A, 0, 9, 15);
+	SeekSum(A, 0, 9, 25);
+	SeekSum(A, 0, 9, 12);
+	SeekSum(A, 0, 9, 13);
 	return 0;
 }	
