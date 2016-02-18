cbb79ea2cf0cf6f07ac2f105fa8cbc0c17d06df5
Firkraag <cntqrxj@gmail.com>
2016-02-18 15:28:08 +0800

    implementation of k-way-merge

---
+++ k_way_merge.py
@@ -0,0 +1,39 @@
+def merge(list1, list2):
+    length1 = len(list1)
+    length2 = len(list2)
+    n = length1 + length2
+    i = 0
+    j = 0
+    k = 0
+    result = [0] * n
+    while i < length1 and j < length2:
+        if list1[i] <= list2[j]:
+            result[k] = list1[i]
+            i += 1
+        else:
+            result[k] = list2[j]
+            j += 1
+        k += 1
+    if i == length1:
+        while j < length2:
+            result[k] = list2[j]
+            k += 1
+            j += 1
+    elif j == length2:
+        while i < length1:
+            result[k] = list1[i]
+            k += 1
+            i += 1
+    return result
+
+
+def k_way_merge(lists):
+    length = len(lists)
+    if length == 1:
+        return lists[0]
+    elif length == 2:
+        return merge(lists[0], lists[1])
+    else:
+        list1 = k_way_merge(lists[0:length / 2])
+        list2 = k_way_merge(lists[length / 2 : length])
+        return merge(list1, list2)
---
+++ k_way_merge_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env ipython
+import unittest
+import random
+from k_way_merge import k_way_merge
+
+class TestKWayMerge(unittest.TestCase):
+    def test_k_way_merge(self):
+		for j in range(0, 10000):
+			A = [random.randint(1, 10000) for i in range(0, 100)]
+			lists = [None] * 10
+			for i in range(0, 10):
+				lists[i] = A[10 * i : (i + 1) * 10]
+				lists[i].sort()
+			A.sort()
+			self.assertEquals(k_way_merge(lists), A)
+
