ad3ee4667f77ec34809307ae91938d2bbdae0da5
wq <smilingwang2@gmail.com>
2018-05-24 23:35:40 +0800

    add bisect_left

--- binary_search.py
+++ binary_search.py
@@ -17,3 +17,31 @@ def binary_search(target, array):
         else:
             return mid
     return -1
+
+
+def bisect_left(array, x, low=0, high=None):
+    """
+    Locate the insertion point for x in a to maintain sorted order.
+    The parameters lo and hi may be used to specify a subset of the list which should be considered;
+    by default the entire list is used. If x is already present in a,
+    the insertion point will be before (to the left of) any existing entries.
+    The return value is suitable for use as the first parameter to list.insert() assuming that a is already sorted.
+
+    The returned insertion point i partitions the array a into two halves so that all(val < x for val in a[lo:i])
+    for the left side and all(val >= x for val in a[i:hi]) for the right side.
+    :param array: sorted list
+    :param x:
+    :param low:
+    :param high:
+    :return:
+    """
+    if high is None:
+        high = len(array)
+    assert low <= high
+    while low < high:
+        mid = (low + high) // 2
+        if array[mid] < x:
+            low = mid + 1
+        else:
+            high = mid
+    return low
--- binary_search_test.py
+++ binary_search_test.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env ipython
 import unittest
 import random
-from binary_search import binary_search
+from binary_search import binary_search, bisect_left
 
 
 class TestBinarySearch(unittest.TestCase):
     def test_binary_search(self):
         for _ in range(100):
-            array = sorted([random.randint(1, 10000) for _ in range(0, 100)])
+            array = sorted([random.randint(1, 10000) for _ in range(100)])
             target = random.randint(1, 10000)
             index = binary_search(target, array)
             if index == -1:
                 self.assertNotIn(target, array)
             else:
                 self.assertEqual(target, array[index])
+
+    def test_bisect_left(self):
+        for length in range(10):
+            array = sorted([random.randint(1, 10) for _ in range(length)])
+            target = random.randint(0, 15)
+            index = bisect_left(array, target)
+            self.assertTrue(all(val < target for val in array[:index]))
+            self.assertTrue(all(val >= target for val in array[index:]))
