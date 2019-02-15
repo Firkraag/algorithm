806ea17928324b401bc04b2233ec2193b938fee8
wq <smilingwang2@gmail.com>
2019-02-15 21:22:30 +0800

    format quicksort

--- quicksort.py
+++ quicksort.py
@@ -1,11 +1,15 @@
 from partition import partition, partition2, partition3, randomized_partition
-def quicksort(A, p, r, partition_method = partition):
+
+
+def quicksort(A, p, r, partition_method=partition):
     if p < r:
         q = partition_method(A, p, r)
         quicksort(A, p, q - 1)
-        quicksort(A, q+1, r)
+        quicksort(A, q + 1, r)
+
+
 def randomized_quicksort(A, p, r):
     if p < r:
         q = randomized_partition(A, p, r)
         randomized_quicksort(A, p, q - 1)
-        randomized_quicksort(A, q+1, r)
+        randomized_quicksort(A, q + 1, r)
