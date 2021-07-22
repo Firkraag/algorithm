46785dd35adf9715b91172a6d087f31848edb96b
Ubuntu <ubuntu@ip-172-31-15-207.ap-northeast-1.compute.internal>
2021-07-23 01:40:00 +0800

    add queue.py

---
+++ binary_add.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+def binary_add(array1, array2):
+    """
+    Consider the problem of adding two n-bit binary integers, stored in two n-element arrays A and B,
+    in big-endian order.
+    The sum of the two integers should be stored in binary form in a (n + 1)-element array C.
+    :param array1: n-element array A
+    :param array2: n-element array B
+    :return: (n + 1)-element array C
+    """
+    assert len(array1) == len(array2)
+    n = len(array1)
+    promote = 0
+    array3 = [0] * (n + 1)
+    for i in range(n - 1, -1, -1):
+        result = array1[i] + array2[i] + promote
+        if result >= 2:
+            result -= 2
+            promote = 1
+        else:
+            promote = 0
+        array3[i + 1] = result
+    array3[0] = promote
+    return array3
