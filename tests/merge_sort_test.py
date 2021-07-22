ccaebf2d7bae5ef562c949bb9bd31691e5b78585
wq <smilingwang2@gmail.com>
2021-07-22 23:50:35 +0800

    conform to PEP8

--- tests/merge_sort_test.py
+++ tests/merge_sort_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import unittest
 import random
-from merge_sort import merge_sort, merge_with_sentinel, merge_without_sentinel, merge_ins_sort, merge_ins_sort2
+from merge_sort import merge_sort, merge_with_sentinel, merge_without_sentinel, merge_ins_sort_bottom_to_top, merge_ins_sort_top_to_bottom
 
 
 class TestMergeSort(unittest.TestCase):
@@ -23,10 +23,10 @@ class TestMergeSort(unittest.TestCase):
         for length in range(100, 200):
             array = [random.randint(1, 10000) for _ in range(length)]
             array_copy = array[:]
-            merge_ins_sort(array, partition=1)
+            merge_ins_sort_bottom_to_top(array, partition=1)
             self.assertEqual(array, sorted(array_copy))
         for length in range(100, 200):
             array = [random.randint(1, 10000) for _ in range(length)]
             array_copy = array[:]
-            merge_ins_sort2(array, sublist_length=15)
+            merge_ins_sort_top_to_bottom(array, sublist_length=15)
             self.assertEqual(array, sorted(array_copy))
